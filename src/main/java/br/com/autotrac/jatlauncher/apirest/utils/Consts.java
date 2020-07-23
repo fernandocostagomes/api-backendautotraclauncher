bed9234b6e59047fe2ea0732a81dcdb024da56d5
Fernando Gomes <fernando.gomes@autotrac.com.br>
2020-07-23 10:38:38 -0300

    Adicionado classe com constantes.

---
+++ src/main/java/br/com/autotrac/jatlauncher/apirest/utils/Consts.java
@@ -0,0 +1,12 @@
+package br.com.autotrac.jatlauncher.apirest.utils;
+
+public class Consts
+{
+   public class TableFlags
+   {
+      public static final int NONE = 0;
+      public static final int APP = 1;
+      public static final int PARAM = 2;
+   }
+
+}
